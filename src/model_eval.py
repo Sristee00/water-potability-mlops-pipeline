07741f0bd315a670acab2eabcfd3f177d84af587
Sristee00 <raisristi818@gmail.com>
2026-04-15 16:15:30 +0000

    model training and evaluation

---
+++ src/model_eval.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pickle
+import json
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+test_data = pd.read_csv("./data/featured/test_featured.csv")
+
+X_test = test_data.drop("Potability", axis=1)
+y_test = test_data["Potability"]
+
+model = pickle.load(open("model.pkl", "rb"))
+
+y_pred = model.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+pre = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1score = f1_score(y_test, y_pred)
+
+metrics_dict = {
+    "accuracy": acc,
+    "precision": pre,
+    "recall": recall,
+    "f1_score": f1score
+}
+
+with open("metrics.json", "w") as file:
+    json.dump(metrics_dict, file, indent=4)
+
+print(metrics_dict)
+print("Model evaluation completed successfully.")
